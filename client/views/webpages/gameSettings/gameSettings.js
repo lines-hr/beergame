3485da6fae72abdaf88a079eb187351cc4524685
Kristijan Lenković <klenkovic@lines.hr>
2016-08-13 23:48:21 +0200

    Fixed autoform and schema for GameSetup, private/public routes, footer.

--- client/views/webpages/gameSettings/gameSettings.js
+++ client/views/webpages/gameSettings/gameSettings.js
@@ -1,3 +1,15 @@
 Template.gameSettings.onCreated(function () {
     Meteor.subscribe("GameSetup");
 });
+
+AutoForm.addHooks('insertGameSettingsForm', {
+    onSubmit: function (insertDoc, updateDoc, currentDoc) {
+        console.log(arguments);
+        return false;
+    },
+    onError: function (name, error, template) {
+        console.log(name + " error:", error);
+    }
+});
+
+SimpleSchema.debug = true;
--- server/publishers/GameSetupPublisher.js
+++ server/publishers/GameSetupPublisher.js
@@ -1,3 +1,9 @@
 Meteor.publish("GameSetup", function () {
-    return GameSetup.find();
+    return GameSetup.find({ $or: [ { setupOwner: this.userId }, { isGlobal: true } ] });
+});
+
+GameSetup.allow({
+    insert: function (userId, doc) {
+        return !!userId && ( ("isGlobal" in doc && !doc.isGlobal) || !("isGlobal" in doc) );
+    }
 });
