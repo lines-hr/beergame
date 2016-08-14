da1c64c3f0ac127499659761cc385b6ffe1f17a2
Kristijan Lenković <klenkovic@lines.hr>
2016-08-14 05:26:53 +0200

    Added Demand Chart.js to GameSetup. Added Chart.js to NPM package.json

--- client/views/webpages/gameSettings/gameSettings.js
+++ client/views/webpages/gameSettings/gameSettings.js
@@ -1,5 +1,62 @@
+var Chart = require("chart");
+
+function updateChart(d) {
+
+    var chartData = new Array({x: 0, y: 0});
+    $.each(d, function (index, value) {
+        if (!value.roundNumber || !value.demand) return false;
+        chartData.push({x: value.roundNumber, y: value.demand});
+    });
+    scatterChart = new Chart($("#demandChart"), {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Demand',
+                data: chartData,
+                lineTension: 0,
+                fill: false,
+                borderColor: "#F00"
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom',
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Game Rounds'
+                    },
+                    ticks: {
+                        min: 0,
+                        stepSize: 1
+                    }
+                }],
+                yAxes: [{
+                    type: 'linear',
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Demand'
+                    },
+                    ticks: {
+                        min: 0
+                    }
+                }]
+            }
+        }
+    });
+}
+
 Template.gameSettings.onCreated(function () {
     Meteor.subscribe("GameSetup");
+});
+
+Template.gameSettings.onRendered(function () {
+
+
+
 });
 
 AutoForm.addHooks('insertGameSettingsForm', {
@@ -9,7 +66,17 @@ AutoForm.addHooks('insertGameSettingsForm', {
     },
     onError: function (name, error, template) {
         console.log(name + " error:", error);
+    },
+    formToDoc: function (doc) {
+        demandSetup = doc.setup.initDemand;
+        if(demandSetup)
+            updateChart(demandSetup);
+
     }
 });
 
 SimpleSchema.debug = true;
+
+
+
+
