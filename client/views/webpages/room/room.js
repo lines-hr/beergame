f047ffdd9806d21f31649eb6e8cae3b8cd205b98
Mislav Miočević <mmiocevic@lines.hr>
2016-08-15 19:01:05 +0200

    In progress with new game templates 8

--- client/views/webpages/room/room.js
+++ client/views/webpages/room/room.js
@@ -3,12 +3,39 @@ Template.room.onCreated(function () {
 });
 
 Template.room.helpers({
-    name: function() {
+    isDefault: function() {
+       return Session.get('templateName') !== null;
+    }
+    /* TODO trenutno je hardcodeano, no za ispis defaultnog templatea mi se čini
+       TODO da ne će ni trebati jer ionako tek kad se ude u igru se dodaju vrijednosti,
+       TODO koje jednostavno dodamo tako da appendamo cijeli Default Beergame
+    ,
 
-        GameSetup.find({title: "Default Beergame"}).forEach(function(obj) {
-            console.log(obj.setup.initStock);
+    defaultTemplate: function() {
+        var defaultSettings = [];
+
+        GameSetup.find({title: "Default Beergame"}).forEach(function (obj) {
+            defaultSettings.push(obj.title);
+            defaultSettings.push(obj.setup.initMaxRounds);
+            defaultSettings.push(obj.setup.initStock);
+            defaultSettings.push(obj.setup.initIncomingDelivery);
+            defaultSettings.push(obj.setup.initIncomingOrder);
+            defaultSettings.push(obj.setup.initBackorder);
+            defaultSettings.push(obj.setup.initRoundLengthShippingDelay);
+            defaultSettings.push(obj.setup.initAmountShippingDelay);
+            defaultSettings.push(obj.setup.initInventoryCost);
+            defaultSettings.push(obj.setup.initBackorderCost);
+            defaultSettings.push(obj.setup.visibleShippings);
+            defaultSettings.push(obj.setup.visibleDemands);
+            defaultSettings.push(obj.setup.allowMessaging);
+            defaultSettings.push(obj.setup.initDemand);
         })
 
-    }
-})
+        return defaultSettings;
+    }*/
+});
+
+Template.room.destroyed = function(){
+    Session.set('templateName', null);
+};
 
--- dbData/GameSetupDefault.js
+++ dbData/GameSetupDefault.js
@@ -1,12 +1,15 @@
 var gameSetupDefaultData = {
-    "title": "Default Beergame",
-    "isGlobal": true,
     "setupOwner": "0v0j3n3k1h4x0r",
+    "isGlobal": true,
+    "title": "Default Beergame",
+    "gamePassword": "",
     "setup": {
         "initStock": 15,
         "initIncomingDelivery": 0,
         "initIncomingOrder": 0,
         "initBackorder": 0,
+        "initRoundLengthShippingDelay": 0,
+        "initAmountShippingDelay": 0,
         "initInventoryCost": 1,
         "initBackorderCost": 2,
         "initMaxRounds": 40,
@@ -54,4 +57,4 @@ var gameSetupDefaultData = {
             "demand": 8
         }, {"roundNumber": 40, "demand": 8}]
     }
-}
+};
